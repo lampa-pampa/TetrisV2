bc9ce77fc11a2d4ffae28e78f5b0e3a47885118b
Lampa Pampa <marek.kandulski@proton.me>
2024-02-09 18:40:21 +0100

    added color and game_state enums

---
+++ includes/color.h
@@ -0,0 +1,15 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+enum class Color
+{
+    red,
+    green,
+    blue,
+    yellow,
+    purple,
+    orange,
+    pink,
+};
+
+#endif
---
+++ includes/game_state.h
@@ -0,0 +1,11 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+enum class GameState
+{
+    in_progress = 0,
+    paused = 1,
+    ended = 2,
+};
+
+#endif
