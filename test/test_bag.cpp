fcd86965f67084ba9c293f3465d5256a8ef2d2d1
Lampa Pampa <marek.kandulski@proton.me>
2024-02-12 12:48:16 +0100

    updated test_bag

--- test/test_bag.cpp
+++ test/test_bag.cpp
@@ -11,7 +11,10 @@ TEST(Bag, test_get_next)
 {
     vector<int> items{0, 1, 2, 3, 4};
     Bag<int> bag{items, unique_ptr<RNG>{new RNGMock()}};
-    auto it{items.begin()};
-    while(it != items.end())
-        ASSERT_EQ(bag.get_next(), *it++);
+    for(int i{0}; i < 2; ++i)
+    {
+        auto it{items.begin()};
+        while(it != items.end())
+            ASSERT_EQ(bag.get_next(), *it++);
+    }
 }
