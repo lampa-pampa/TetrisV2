f56e68c88c9bec8e4cc56fd9022e0e6afd47f0ee
Lampa Pampa <marek.kandulski@proton.me>
2024-02-27 21:42:28 +0100

    implemented pause and resume functions

--- src/game_controller.cpp
+++ src/game_controller.cpp
@@ -3,15 +3,17 @@
 
 void GameController::pause_pressed()
 {
-    switch (this->game.get_state())
+    switch(this->game.get_state())
     {
         case GameState::in_progress:
             this->timer.stop();
             this->game.set_state(GameState::paused);
+            this->game.pause();
             break;
         case GameState::paused:
             this->timer.start();
             this->game.set_state(GameState::in_progress);
+            this->game.resume();
             break;
         default:
             break;
