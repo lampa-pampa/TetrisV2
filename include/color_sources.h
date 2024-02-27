9b6d43ea49c40331975504fa72cc88f9edb8e610
Lampa Pampa <marek.kandulski@proton.me>
2024-02-27 20:17:12 +0100

    added color_sources

---
+++ include/color_sources.h
@@ -0,0 +1,18 @@
+#ifndef COLOR_SOURCES_H
+#define COLOR_SOURCES_H
+
+#include "color.h"
+#include <vector>
+
+const std::vector<Color> color_sources
+{
+    Color::red,
+    Color::green,
+    Color::yellow,
+    Color::blue,
+    Color::purple,
+    Color::orange,
+    Color::pink,
+};
+
+#endif
