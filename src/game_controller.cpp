8961e342ba404d6013ecd79425c5ad247aecb807
Lampa Pampa <marek.kandulski@proton.me>
2024-02-27 23:19:35 +0100

    replaced switch with if, removed setting state

--- src/game_controller.cpp
+++ src/game_controller.cpp
@@ -1,21 +1,18 @@
 #include "game_controller.h"
 #include "game_state.h"
 
-void GameController::pause_pressed()
+void GameController::handle_pause_pressed()
 {
-    switch(this->game.get_state())
+    const GameState game_state{this->game.get_state()};
+
+    if(game_state == GameState::in_progress)
     {
-        case GameState::in_progress:
-            this->timer.stop();
-            this->game.set_state(GameState::paused);
-            this->game.pause();
-            break;
-        case GameState::paused:
-            this->timer.start();
-            this->game.set_state(GameState::in_progress);
-            this->game.resume();
-            break;
-        default:
-            break;
+        this->timer.stop();
+        this->game.pause();
+    }
+    else if(game_state == GameState::paused)
+    {
+        this->timer.start();
+        this->game.resume();
     }
 }
