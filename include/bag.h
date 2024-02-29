d4d0fbf4e0386f93adad5c262b381955e7889edd
Lampa Pampa <marek.kandulski@proton.me>
2024-02-29 01:02:32 +0100

    make some functions inline in bag

--- include/bag.h
+++ include/bag.h
@@ -15,28 +15,22 @@ class Bag final
     int current_index;
     Rng& rng;
 
-    void shuffle_items(std::vector<T>& items, Rng& rng);
+    void shuffle_items(std::vector<T>& items, Rng& rng)
+    {
+        for (const auto& i : boost::irange<int>(items.size() - 1, -1, -1))
+            std::swap(items[i], items[rng.random(i + 1)]);
+    }
 
 public:
-    Bag(const std::vector<T>& items, Rng& rng);
-
+    Bag(const std::vector<T>& items, Rng& rng)
+    :
+        items(items),
+        current_index(0),
+        rng(rng)
+    {}
+    
     T get_next();
 };
-
-template<typename T>
-void Bag<T>::shuffle_items(std::vector<T>& items, Rng& rng)
-{
-    for (const auto& i : boost::irange<int>(items.size() - 1, -1, -1))
-        std::swap(items[i], items[rng.random(i + 1)]);
-}
-
-template<typename T>
-Bag<T>::Bag(const std::vector<T>& items, Rng& rng)
-:
-    items(items),
-    current_index(0),
-    rng(rng)
-{}
 
 template<typename T>
 T Bag<T>::get_next()
