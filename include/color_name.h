4f5d05a0d83f1ebcf2f0f87a93a3cdc1cdc74053
Lampa Pampa <marek.kandulski@proton.me>
2024-03-03 00:31:25 +0100

    renamed color to color_name removed friend stream operator, updated color names

---
+++ include/color_name.h
@@ -0,0 +1,37 @@
+#ifndef INCLUDE_COLOR_NAME_H
+#define INCLUDE_COLOR_NAME_H
+
+#include <map>
+
+namespace Tetris
+{
+
+enum class ColorName
+{
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    purple,
+    orange,
+    pink,
+    white,
+};
+
+const std::map<ColorName, int> color_name_to_code
+{
+    {ColorName::black, 0},
+    {ColorName::red, 1},
+    {ColorName::green, 2},
+    {ColorName::blue, 3},
+    {ColorName::yellow, 4},
+    {ColorName::purple, 5},
+    {ColorName::orange, 6},
+    {ColorName::pink, 7},
+    {ColorName::white, 8},
+};
+
+}
+
+#endif
