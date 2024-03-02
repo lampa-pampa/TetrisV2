d7d150c8e2984c11949c088340dd627203fa1871
Lampa Pampa <marek.kandulski@proton.me>
2024-03-03 00:35:25 +0100

    ...

--- include/matrix.h
+++ include/matrix.h
@@ -1,18 +1,20 @@
-#ifndef INCLUDE_MATRIX
-#define INCLUDE_MATRIX
+#ifndef INCLUDE_MATRIX_DISPLAY
+#define INCLUDE_MATRIX_DISPLAY
 
 #include <vector>
 
 namespace Tetris
 {
 
-class Matrix
+class MatrixDisplay
 {
-    using Pixels = std::vector<std::vector<int>>;
+    using ColorCodes = std::vector<std::vector<int>>;
 
 public:   
-    virtual void refresh_pixels(Pixels pixels) = 0;
-    virtual ~Matrix() = default;
+    virtual void refresh(const ColorCodes& color_codes) = 0;
+    virtual int get_width() const = 0;
+    virtual int get_height() const = 0;
+    virtual ~MatrixDisplay() = default;
 };
 
 }
