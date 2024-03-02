bbcc61cd91aebf75aae68fb079a4fece30284240
Lampa Pampa <marek.kandulski@proton.me>
2024-03-03 00:27:14 +0100

    added assertion to get_ncurses_color

--- src/ncurses_colors.cpp
+++ src/ncurses_colors.cpp
@@ -1,5 +1,7 @@
 #include "ncurses_colors.h"
 
+#include <cassert>
+
 #include <ncurses.h>
 
 namespace Tetris
@@ -10,7 +12,10 @@ int NCursesColors::get_ncurses_color(int color_code)
     auto it{this->color_to_pair.find(color_code)};
     if (it == this->color_to_pair.end())
     {
-        const int ncurses_color{this->color_to_ncurses_color.at(color)};
+        assert(
+            this->color_code_to_ncurses_color.find(color_code)
+                != this->color_code_to_ncurses_color.end()
+        );
         const int ncurses_color{
             this->color_code_to_ncurses_color.at(color_code)
         };
