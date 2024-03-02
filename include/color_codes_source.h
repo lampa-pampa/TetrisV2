ebd38b28310ca53199c74bc7c1d1e8aa1afe470d
Lampa Pampa <marek.kandulski@proton.me>
2024-03-03 00:30:28 +0100

    renamed color_sources to color_codes_source

---
+++ include/color_codes_source.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDE_COLOR_CODES_SOURCE_H
+#define INCLUDE_COLOR_CODES_SOURCE_H
+
+#include <vector>
+
+namespace Tetris
+{
+
+const std::vector color_codes_source
+{
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+};
+
+}
+
+#endif
