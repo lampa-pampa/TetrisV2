fd99ec30e9eb46ff39a5d4097985147420732e9b
Lampa Pampa <marek.kandulski@proton.me>
2024-03-04 01:44:13 +0100

    ...

--- include/board.h
+++ include/board.h
@@ -19,7 +19,8 @@ public:
     virtual void clear_cubes(const std::vector<Cube>& cubes) = 0;
     virtual int remove_lines_and_compress(int from_y, int to_y) = 0;
     virtual int get_width() const = 0;
-    virtual const CubeMatrix& get_cubes() const = 0; 
+    virtual const CubeMatrix& get_cubes() const = 0;
+    virtual CubeMatrix get_cubes(int offset) const = 0;
     virtual ~Board() = default;
 };
 
