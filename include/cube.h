c60d2625f6e31e021ec2756a04684c0f033d20db
Lampa Pampa <marek.kandulski@proton.me>
2024-03-05 09:05:26 +0100

    added constexpr black_color_code, removed bool is_ghost

--- include/cube.h
+++ include/cube.h
@@ -10,9 +10,10 @@ namespace Tetris
 
 struct Cube final
 {
+    static constexpr int black_color_code{0};
+
     Vector2 position;
     int color_code;
-    bool is_ghost;
 
     friend std::ostream& operator<<(std::ostream& os, const Cube& cube)
     {
@@ -20,39 +21,31 @@ struct Cube final
             << ", " << cube.color_code << "}";
     }
 
-    Cube(int x, int y, int color_code, bool is_ghost)
-    :
-        position{x, y},
-        color_code{color_code},
-        is_ghost{is_ghost}
-    {}
-
     Cube(int x, int y, int color_code)
     :
-        Cube{x, y, color_code, false}
+        position{x, y},
+        color_code{color_code}
     {}
 
     Cube(int x, int y)
     :
-        Cube{x, y, 0, false}
+        Cube{x, y, black_color_code}
     {}
 
     bool operator==(const Cube& other) const
     {
         return this->position == other.position
-            and this->is_ghost == other.is_ghost
             and this->color_code == other.color_code;
     }
 
     bool empty() const
     {
-        return this->color_code == 0;
+        return this->color_code == black_color_code;
     }
 
     void clear()
     {
-        this->color_code = 0;
-        this->is_ghost = false;
+        this->color_code = black_color_code;
     }
 };
 
