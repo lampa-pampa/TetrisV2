e8e94b65a1b1efeec1ec9b0f9fd050b1885d9108
Lampa Pampa <marek.kandulski@proton.me>
2024-03-05 09:04:08 +0100

    ...

--- src/ncurses_colors.cpp
+++ src/ncurses_colors.cpp
@@ -12,11 +12,11 @@ int NCursesColors::get_ncurses_color(int color_code)
     auto it{this->color_to_pair.find(color_code)};
     if (it == this->color_to_pair.end())
     {
-        assert(
-            this->color_code_to_ncurses_color.find(color_code)
+        assert(this->color_code_to_ncurses_color.find(color_code)
             != this->color_code_to_ncurses_color.end());
         const int ncurses_color{
-            this->color_code_to_ncurses_color.at(color_code)};
+            this->color_code_to_ncurses_color.at(color_code)
+        };
         ::init_pair(ncurses_color, ncurses_color, COLOR_BLACK);
         it = this->color_to_pair.insert({color_code, ncurses_color}).first;
     }
