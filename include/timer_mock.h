579aac29ea7672505e077d29228b19dbc4aff282
Lampa Pampa <marek.kandulski@proton.me>
2024-03-07 23:53:53 +0100

    refactored timer_mock

--- include/timer_mock.h
+++ include/timer_mock.h
@@ -1,25 +1,26 @@
 #ifndef INCLUDE_TIMER_MOCK_H
 #define INCLUDE_TIMER_MOCK_H
 
+#include "timer.h"
+
 #include <functional>
 
 #include <boost/signals2.hpp>
-
-#include "timer.h"
 
 namespace Tetris
 {
 
 class TimerMock final: public Timer
 {
-    using Signal = boost::signals2::signal<void()>;
-    
-    Signal timeout;
-
 public:
     void start() override {}
     void stop() override {}
     void connect_timeout(const std::function<void()>& handler) override {}
+
+private:
+    using Signal = boost::signals2::signal<void()>;
+    
+    Signal timeout;
 };
 
 }
