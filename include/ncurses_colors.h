436c1a3db5ef4ae40bb1d6ccb1190bcef23bb40f
Lampa Pampa <marek.kandulski@proton.me>
2024-03-08 08:55:24 +0100

    refactored ncurses_colors

--- include/ncurses_colors.h
+++ include/ncurses_colors.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDE_NCURSES_COLORS_H
 #define INCLUDE_NCURSES_COLORS_H
 
+#include <cassert>
 #include <map>
 
 #include <ncurses.h>
@@ -10,6 +11,18 @@ namespace Tetris
 
 class NCursesColors final
 {
+public:
+    int get_ncurses_color(int color_code)
+    {
+        auto it{this->color_to_pair.find(color_code)};
+        if (it == this->color_to_pair.end())
+            it = this->create_color_pair(color_code);
+        return it->second;
+    }
+
+private:
+    using ColorPair = std::_Rb_tree_iterator<std::pair<const int, int>>;
+    
     const std::map<int, int> color_code_to_ncurses_color
     {
         {0, 16},
@@ -24,9 +37,14 @@ class NCursesColors final
     };
     
     std::map<int, int> color_to_pair;
-    
-public:
-    int get_ncurses_color(int color_code);
+
+    ColorPair create_color_pair(int color_code)
+    {
+        auto it{color_code_to_ncurses_color.find(color_code)};
+        assert(it != this->color_code_to_ncurses_color.end());
+        ::init_pair(it->second, it->second, COLOR_BLACK);
+        return this->color_to_pair.insert({color_code, it->second}).first;
+    }
 };
 
 }
