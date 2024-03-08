70b6f33f3635dfa3e2f5ee6c7e062b7e162ae7da
Lampa Pampa <marek.kandulski@proton.me>
2024-03-08 08:34:22 +0100

    updated score_counter_impl test

--- test/test_score_counter_impl.cpp
+++ test/test_score_counter_impl.cpp
@@ -1,28 +1,64 @@
+#include "score_counter_impl.h"
+
+#include <utility>
+#include <vector>
+
+#include <boost/range/irange.hpp>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "score_counter_impl.h"
-
+using boost::irange;
+using std::pair;
+using std::vector;
 using testing::Eq;
 using Tetris::ScoreCounterImpl;
 
 TEST(ScoreCounterImpl, test_count_score_for_lines)
 {
-    ScoreCounterImpl score_counter{2, 1, 1};
-    
-    ASSERT_THAT(score_counter.count_score_for_lines(10), Eq(20));
+    const vector<pair<ScoreCounterImpl, int>> score_counter_to_expected{
+        { {2, 1, 2}, 2},
+        { {10, 3, 7}, 10},
+        { {15, 2, 9}, 15},
+    };
+
+    for(const auto& pair : score_counter_to_expected)
+    {
+        for(const auto& lines_quantity : irange(3))
+        {
+            const int actual{pair.first.count_score_for_lines(lines_quantity)};
+            
+            ASSERT_THAT(actual, Eq(pair.second * lines_quantity));
+        }
+    }
 }
 
 TEST(ScoreCounterImpl, test_count_score_for_soft_drop)
 {
-    ScoreCounterImpl score_counter{1, 3, 1};
-    
-    ASSERT_THAT(score_counter.count_score_for_soft_drop(), Eq(3));
+    const vector<pair<ScoreCounterImpl, int>> score_counter_to_expected{
+        { {2, 1, 2}, 1},
+        { {10, 3, 7}, 3},
+        { {15, 2, 9}, 2},
+    };
+
+    for(const auto& pair : score_counter_to_expected)
+        ASSERT_THAT(pair.first.count_score_for_soft_drop(), Eq(pair.second));
 }
 
 TEST(ScoreCounterImpl, test_count_score_for_hard_drop)
 {
-    ScoreCounterImpl score_counter{1, 1, 5};
-    
-    ASSERT_THAT(score_counter.count_score_for_hard_drop(10), Eq(50));
+    const vector<pair<ScoreCounterImpl, int>> score_counter_to_expected{
+        { {2, 1, 2}, 2},
+        { {10, 3, 7}, 7},
+        { {15, 2, 9}, 9},
+    };
+
+    for(const auto& pair : score_counter_to_expected)
+    {
+        for(const auto& distance : irange(10))
+        {
+            const int actual{pair.first.count_score_for_hard_drop(distance)};
+            
+            ASSERT_THAT(actual, Eq(pair.second * distance));
+        }
+    }
 }
