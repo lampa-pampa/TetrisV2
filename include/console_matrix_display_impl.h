1d4843c20c67755f07b7da94ddd97d117b0aa5ad
Lampa Pampa <marek.kandulski@proton.me>
2024-03-12 08:45:41 +0100

    moved console_matrix_display_impl methods to source file

--- include/console_matrix_display_impl.h
+++ include/console_matrix_display_impl.h
@@ -5,8 +5,6 @@
 
 #include <ncurses.h>
 
-#include <boost/range/irange.hpp>
-
 #include "ncurses_colors.h"
 #include "vector_2.h"
 
@@ -17,32 +15,14 @@ class ConsoleMatrixDisplayImpl final: public MatrixDisplay
 {  
 public:
     ConsoleMatrixDisplayImpl(
-        int width,
-        int height,
-        NCursesColors ncurses_colors)
-    :
-        width{width},
-        height{height},
-        ncurses_colors{ncurses_colors}
-    {
-        this->setup_ncurses_window();
-        this->setup_ncurses_keyboard();
-    }
+        int width, int height, NCursesColors ncurses_colors);
 
     ~ConsoleMatrixDisplayImpl()
     {
         ::endwin();
     }
 
-    void refresh(const ColorCodeMatrix& color_codes) override
-    {
-        for (const auto& y : boost::irange(this->height))
-        {
-            for (const auto& x : boost::irange(this->width))
-                this->refresh_pixel({x, y}, color_codes[y][x]);
-        }
-        ::wrefresh(this->window);
-    }
+    void refresh(const ColorCodeMatrix& color_codes) override;
 
     int get_width() const override
     {
@@ -70,57 +50,16 @@ private:
     ::WINDOW* window;
     NCursesColors ncurses_colors;
 
-    Vector2 get_console_size() const
-    {
-        Vector2 size{};
-        getmaxyx(stdscr, size.y, size.x);
-        return size;
-    }
+    Vector2 get_console_size() const;
+    Vector2 compute_window_position() const;
+    void create_window();
+    void print_colored(Vector2 position, int ncurses_color);
+    void refresh_pixel(Vector2 position, int color_code);
+    void setup_ncurses_window();
 
     Vector2 compute_window_size() const
     {
         return {this->width * pixel_width, this->height * pixel_height};
-    }
-
-    Vector2 compute_window_position() const
-    {
-        const Vector2 console_size{this->get_console_size()};
-        const Vector2 window_size{this->compute_window_size()};
-        assert(console_size >= window_size);
-        return {console_size / 2 - window_size / 2};
-    }
-
-    void create_window()
-    {
-        const Vector2 size{this->compute_window_size()};
-        const Vector2 position{this->compute_window_position()};
-        this->window = ::newwin(
-            size.y, size.x, position.y, position.x);
-        ::refresh();
-    }
-
-    void print_colored(Vector2 position, int ncurses_color)
-    {
-        ::wattron(this->window, COLOR_PAIR(ncurses_color));
-        ::mvwprintw(this->window, position.y, position.x, "%lc", pixel_char);
-        ::wattroff(this->window, COLOR_PAIR(ncurses_color));
-    }
-
-    void refresh_pixel(Vector2 position, int color_code)
-    {
-        const int ncurses_color{
-            this->ncurses_colors.get_ncurses_color(color_code)
-        };
-        this->print_colored(
-            position * Vector2{pixel_width, pixel_height}, ncurses_color);
-    }
-
-    void setup_ncurses_window()
-    {
-        ::setlocale(LC_ALL, "");
-        ::initscr();
-        ::start_color();
-        this->create_window();
     }
 
     void setup_ncurses_keyboard()
---
+++ src/console_matrix_display_impl.cpp
@@ -0,0 +1,86 @@
+#include "console_matrix_display_impl.h"
+
+#include <boost/range/irange.hpp>
+#include <ncurses.h>
+
+#include "ncurses_colors.h"
+#include "vector_2.h"
+
+using boost::irange;
+
+namespace Tetris
+{
+
+ConsoleMatrixDisplayImpl::ConsoleMatrixDisplayImpl(
+    int width, int height, NCursesColors ncurses_colors)
+:
+    width{width},
+    height{height},
+    ncurses_colors{ncurses_colors}
+{
+    this->setup_ncurses_window();
+    this->setup_ncurses_keyboard();
+}
+
+void ConsoleMatrixDisplayImpl::refresh(const ColorCodeMatrix& color_codes)
+{
+    for (const auto& y : irange(this->height))
+    {
+        for (const auto& x : irange(this->width))
+            this->refresh_pixel({x, y}, color_codes[y][x]);
+    }
+    ::wrefresh(this->window);
+}
+
+//-----------------------------------------------------------------------
+
+Vector2 ConsoleMatrixDisplayImpl::get_console_size() const
+{
+    Vector2 size{};
+    getmaxyx(stdscr, size.y, size.x);
+    return size;
+}
+
+Vector2 ConsoleMatrixDisplayImpl::compute_window_position() const
+{
+    const Vector2 console_size{this->get_console_size()};
+    const Vector2 window_size{this->compute_window_size()};
+    assert(console_size >= window_size);
+    return {console_size / 2 - window_size / 2};
+}
+
+void ConsoleMatrixDisplayImpl::create_window()
+{
+    const Vector2 size{this->compute_window_size()};
+    const Vector2 position{this->compute_window_position()};
+    this->window = ::newwin(
+        size.y, size.x, position.y, position.x);
+    ::refresh();
+}
+
+void ConsoleMatrixDisplayImpl::print_colored(
+    Vector2 position, int ncurses_color)
+{
+    ::wattron(this->window, COLOR_PAIR(ncurses_color));
+    ::mvwprintw(this->window, position.y, position.x, "%lc", pixel_char);
+    ::wattroff(this->window, COLOR_PAIR(ncurses_color));
+}
+
+void ConsoleMatrixDisplayImpl::refresh_pixel(Vector2 position, int color_code)
+{
+    const int ncurses_color{
+        this->ncurses_colors.get_ncurses_color(color_code)
+    };
+    this->print_colored(
+        position * Vector2{pixel_width, pixel_height}, ncurses_color);
+}
+
+void ConsoleMatrixDisplayImpl::setup_ncurses_window()
+{
+    ::setlocale(LC_ALL, "");
+    ::initscr();
+    ::start_color();
+    this->create_window();
+}
+
+}
