180809038724c702ecddcd0dadfefa04b4ae9412
Lampa Pampa <marek.kandulski@proton.me>
2024-03-12 08:17:06 +0100

    moved brick methods to source file

--- include/brick.h
+++ include/brick.h
@@ -1,20 +1,16 @@
 #ifndef INCLUDE_BRICK_H
 #define INCLUDE_BRICK_H
 
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
+#include <ostream>
 #include <vector>
-
-#include <boost/range/irange.hpp>
 
 #include "cube.h"
 #include "vector_2.h"
 
 namespace Tetris
 {
-inline std::ostream& operator<<(
-    std::ostream& os, const std::vector<Cube>& cubes);
+
+std::ostream& operator<<(std::ostream& os, const std::vector<Cube>& cubes);
 
 struct Brick final
 {
@@ -22,6 +18,12 @@ struct Brick final
 
     std::vector<Cube> cubes;
     Vector2 rotation_offset;
+
+    static Brick get_translated(const Brick& brick, Vector2 position);
+    static Vector2 get_rotated_position(
+        Vector2 position, Vector2 rotation_offset, int quarters_rotation);
+    static Brick get_rotated(const Brick& brick, int quarters_rotation);
+    static Brick get_colored(const Brick& brick, int color_code);
 
     friend inline std::ostream& operator<<(
         std::ostream& os, const Brick& brick)
@@ -33,45 +35,6 @@ struct Brick final
     static int compute_next_rotation(int rotation, int d_q)
     {
         return (rotation + d_q + rotation_quantity) % rotation_quantity;
-    }
-
-    static Brick get_colored(const Brick& brick, int color_code)
-    {
-        Brick colored_brick{brick};
-        for (auto& cube : colored_brick.cubes)
-            cube.color_code = color_code;
-        return colored_brick;
-    }
-
-    static Brick get_translated(const Brick& brick, Vector2 position)
-    {
-        Brick translated_brick{brick};
-        for (auto& cube : translated_brick.cubes)
-            cube.position += position;
-        return translated_brick;
-    }
-
-    static Vector2 get_rotated_position(
-        Vector2 position, Vector2 rotation_offset, int quarters_rotation)
-    {
-        for (const auto& i : boost::irange(quarters_rotation))
-        {
-            std::swap(position.x, position.y);
-            position.x *= -1;
-            position += rotation_offset;
-        }
-        return position;
-    }
-
-    static Brick get_rotated(const Brick& brick, int quarters_rotation)
-    {
-        Brick rotated_brick{brick};
-        for (auto& cube : rotated_brick.cubes)
-        {
-            cube.position = get_rotated_position(
-                cube.position, brick.rotation_offset, quarters_rotation);
-        }
-        return rotated_brick;
     }
 
     static Brick get_transformed(
@@ -86,12 +49,14 @@ struct Brick final
         rotation_offset{rotation_offset}
     {}
         
-    Brick(std::vector<Cube> cubes)
-    :
-        Brick{cubes, {0, 0}}
-    {}
-
-    Brick() = default;
+    Brick(std::vector<Cube> cubes): Brick{cubes, {0, 0}}{}
+    Brick(): Brick{{}}{};
+    int get_min_x() const;
+    int get_max_x() const;
+    int get_min_y() const;
+    int get_max_y() const;
+    int get_width() const;
+    int get_height() const;
 
     bool operator==(const Brick& other) const
     {
@@ -103,81 +68,7 @@ struct Brick final
     {
         return this->cubes.empty();
     }
-
-    int get_min_x() const
-    {
-        if (this->cubes.empty())
-            return 0;
-        return std::min_element(
-            this->cubes.begin(),
-            this->cubes.end(),
-            [](const Cube& a, const Cube& b){
-                return a.position.x < b.position.x;}
-        )->position.x;
-    }
-
-    int get_max_x() const
-    {
-        if (this->cubes.empty())
-            return 0;
-        return std::max_element(
-            this->cubes.begin(),
-            this->cubes.end(),
-            [](const Cube& a, const Cube& b){
-                return a.position.x < b.position.x;}
-        )->position.x;
-    }
-
-    int get_min_y() const
-    {
-        if (this->cubes.empty())
-            return 0;
-        return std::min_element(
-            this->cubes.begin(),
-            this->cubes.end(),
-            [](const Cube& a, const Cube& b){
-                return a.position.y < b.position.y;}
-        )->position.y;
-    }
-
-    int get_max_y() const
-    {
-        if (this->cubes.empty())
-            return 0;
-        return std::max_element(
-            this->cubes.begin(),
-            this->cubes.end(),
-            [](const Cube& a, const Cube& b){
-                return a.position.y < b.position.y;}
-        )->position.y;
-    }
-
-    int get_width() const
-    {
-        if (this->empty())
-            return 0;
-        return std::abs(this->get_max_x() - this->get_min_x()) + 1;
-    }
-
-    int get_height() const
-    {
-        if (this->empty())
-            return 0;
-        return std::abs(this->get_max_y() - this->get_min_y()) + 1;
-    }
 };
-
-inline std::ostream& operator<<(
-    std::ostream& os, const std::vector<Cube>& cubes)
-{
-    for (const auto& cube : cubes)
-    {
-        os << cube;
-        if (&cube != &cubes.back())
-            os << ", ";
-    }
-    return os;
-}
 
 }
 
---
+++ src/brick.cpp
@@ -0,0 +1,134 @@
+#include "brick.h"
+
+#include <algorithm>
+#include <boost/range/irange.hpp>
+#include <cstdlib>
+#include <ostream>
+#include <vector>
+
+#include "cube.h"
+#include "vector_2.h"
+
+using boost::irange;
+using std::max_element;
+using std::min_element;
+using std::ostream;
+using std::swap;
+using std::vector;
+
+namespace Tetris
+{
+
+Brick Brick::get_colored(const Brick& brick, int color_code)
+{
+    Brick colored_brick{brick};
+    for (auto& cube : colored_brick.cubes)
+        cube.color_code = color_code;
+    return colored_brick;
+}
+
+Brick Brick::get_translated(const Brick& brick, Vector2 position)
+{
+    Brick translated_brick{brick};
+    for (auto& cube : translated_brick.cubes)
+        cube.position += position;
+    return translated_brick;
+}
+
+Vector2 Brick::get_rotated_position(
+    Vector2 position, Vector2 rotation_offset, int quarters_rotation)
+{
+    for (const auto& i : irange(quarters_rotation))
+    {
+        swap(position.x, position.y);
+        position.x *= -1;
+        position += rotation_offset;
+    }
+    return position;
+}
+
+Brick Brick::get_rotated(const Brick& brick, int quarters_rotation)
+{
+    Brick rotated_brick{brick};
+    for (auto& cube : rotated_brick.cubes)
+    {
+        cube.position = get_rotated_position(
+            cube.position, brick.rotation_offset, quarters_rotation);
+    }
+    return rotated_brick;
+}
+
+int Brick::get_min_x() const
+{
+    if (this->cubes.empty())
+        return 0;
+    return min_element(
+        this->cubes.begin(),
+        this->cubes.end(),
+        [](const Cube& a, const Cube& b){
+            return a.position.x < b.position.x;}
+    )->position.x;
+}
+
+int Brick::get_max_x() const
+{
+    if (this->cubes.empty())
+        return 0;
+    return max_element(
+        this->cubes.begin(),
+        this->cubes.end(),
+        [](const Cube& a, const Cube& b){
+            return a.position.x < b.position.x;}
+    )->position.x;
+}
+
+int Brick::get_min_y() const
+{
+    if (this->cubes.empty())
+        return 0;
+    return min_element(
+        this->cubes.begin(),
+        this->cubes.end(),
+        [](const Cube& a, const Cube& b){
+            return a.position.y < b.position.y;}
+    )->position.y;
+}
+
+int Brick::get_max_y() const
+{
+    if (this->cubes.empty())
+        return 0;
+    return max_element(
+        this->cubes.begin(),
+        this->cubes.end(),
+        [](const Cube& a, const Cube& b){
+            return a.position.y < b.position.y;}
+    )->position.y;
+}
+
+int Brick::get_width() const
+{
+    if (this->empty())
+        return 0;
+    return abs(this->get_max_x() - this->get_min_x()) + 1;
+}
+
+int Brick::get_height() const
+{
+    if (this->empty())
+        return 0;
+    return abs(this->get_max_y() - this->get_min_y()) + 1;
+}
+
+ostream& operator<<(ostream& os, const vector<Cube>& cubes)
+{
+    for (const auto& cube : cubes)
+    {
+        os << cube;
+        if (&cube != &cubes.back())
+            os << ", ";
+    }
+    return os;
+}
+
+}
