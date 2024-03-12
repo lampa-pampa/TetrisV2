e7fc1a248512850de1fa76fcf5bcf491b1728e45
Lampa Pampa <marek.kandulski@proton.me>
2024-03-12 09:00:13 +0100

    moved ncurses_colors methods to source file

---
+++ src/ncurses_colors.cpp
@@ -0,0 +1,27 @@
+#include "ncurses_colors.h"
+
+#include <cassert>
+#include <ncurses.h>
+
+namespace Tetris
+{
+
+int NCursesColors::get_ncurses_color(int color_code)
+{
+    auto it{this->color_to_pair.find(color_code)};
+    if (it == this->color_to_pair.end())
+        it = this->create_color_pair(color_code);
+    return it->second;
+}
+
+//----------------------------------------------------
+
+NCursesColors::ColorPair NCursesColors::create_color_pair(int color_code)
+{
+    auto it{color_code_to_ncurses_color.find(color_code)};
+    assert(it != this->color_code_to_ncurses_color.end());
+    ::init_pair(it->second, it->second, COLOR_BLACK);
+    return this->color_to_pair.insert({color_code, it->second}).first;
+}
+
+}
