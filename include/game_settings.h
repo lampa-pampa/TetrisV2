602970463d2fbb833ffdf7bbf2e048dd615c44d5
Lampa Pampa <marek.kandulski@proton.me>
2024-03-13 12:46:17 +0100

    added game settings struct

---
+++ include/game_settings.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_GAME_SETTINGS_H
+#define INCLUDE_GAME_SETTINGS_H
+
+namespace Tetris
+{
+
+struct GameSettings final
+{
+    bool generate_ghost;
+    bool locking_hard_drop;
+};
+
+}
+
+#endif
