f31e51eff8b6c7949fd7759750280233da8011d7
Lampa Pampa <marek.kandulski@proton.me>
2024-03-18 20:48:21 +0100

    added iv_color (id, value color struct)

---
+++ include/ui/iv_color.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_UI_IV_COLOR_H
+#define INCLUDE_UI_IV_COLOR_H
+
+#include <cstdint>
+
+namespace Tetris::Ui
+{
+
+struct IvColor
+{
+    uint_fast8_t id;
+    uint_fast8_t value;
+
+    IvColor(uint_fast8_t id, uint_fast8_t value = 0xff)
+    :
+        id{id},
+        value{value}
+    {}
+};
+
+}
+
+#endif
