1ea46b62eed510746a720e02bd4df2ae63102d4c
Lampa Pampa <marek.kandulski@proton.me>
2024-03-20 17:12:23 +0100

    cleaned code

--- include/ui/progress_bar.h
+++ include/ui/progress_bar.h
@@ -17,18 +17,18 @@ public:
         std::vector<Rectangle>, std::vector<Rectangle>>;
 
     constexpr ProgressBar(
-    Vector2 position,
-    int segment_width,
-    int segment_height,
-    int height,
-    int separator)
-:
-    position{position},
-    segment_width{segment_width},
-    segment_height{segment_height},
-    height{height},
-    separator{separator}
-{}
+        Vector2 position,
+        int segment_width,
+        int segment_height,
+        int height,
+        int separator)
+    :
+        position{position},
+        segment_width{segment_width},
+        segment_height{segment_height},
+        height{height},
+        separator{separator}
+    {}
 
     Segments create_segments(int value) const;
 
