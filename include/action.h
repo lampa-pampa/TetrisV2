c9e68f11e7710ae8d323e37ac119ff35673bc1e2
Lampa Pampa <marek.kandulski@proton.me>
2024-03-20 16:52:17 +0100

    added friend ostream operetor and map to string

--- include/action.h
+++ include/action.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_ACTION_H
 #define INCLUDE_ACTION_H
 
+#include <map>
+#include <ostream>
+#include <string>
+
 namespace Tetris
 {
 
@@ -15,6 +19,26 @@ enum class Action
     no_locking_hard_drop,
     hold,
 };
+
+namespace
+{
+    const std::map<Action, std::string> action_to_name
+    {
+        {Action::move_left, "move_left"},
+        {Action::move_right, "paused"},
+        {Action::rotate_clockwise, "rotate_clockwise"},
+        {Action::soft_drop, "soft_drop"},
+        {Action::locking_hard_drop, "locking_hard_drop"},
+        {Action::rotate_counter_clockwise, "rotate_counter_clockwise"},
+        {Action::no_locking_hard_drop, "no_locking_hard_drop"},
+        {Action::hold, "hold"},
+    };
+}
+
+inline std::ostream& operator<<(std::ostream& os, Action action)
+{
+    return os << "Action::" << action_to_name.at(action);
+}
 
 }
 
