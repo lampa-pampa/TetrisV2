132ff05b4e6b2507d6ec2098a9a9e99745d0d1d9
Lampa Pampa <marek.kandulski@proton.me>
2024-03-20 21:10:38 +0100

    updated board test

--- test/test_board_impl.cpp
+++ test/test_board_impl.cpp
@@ -132,71 +132,45 @@ TEST(BoardImpl, brick_is_valid)
         ASSERT_THAT(board.brick_is_valid(pair.first), Eq(pair.second));
 }
 
-TEST(BoardImpl, get_width)
-{
-    const vector<pair<BoardImpl, int>> board_to_expected{
-        { {{10, 20}}, 10 },
-        { {{2, 10}}, 2 },
-        { {{20, 20}}, 20 },
-    };
-
-    for (const auto& pair : board_to_expected)
-        ASSERT_THAT(pair.first.get_width(), Eq(pair.second));
-}
-
-TEST(BoardImpl, get_offset)
-{
-    const vector<pair<BoardImpl, int>> board_to_expected{
-        { {{10, 20}}, 0 },
-        { {{10, 20}, 2}, 2 },
-        { {{10, 20}, 5}, 5 },
-    };
-
-    for (const auto& pair : board_to_expected)
-        ASSERT_THAT(pair.first.get_offset(), Eq(pair.second));
-}
-
 TEST(BoardImpl, get_visible_cubes)
 {
-    const vector<Cube> initial_cubes{ {0, 0, 1}, {1, 0, 1}, {0, 1, 1} };
-    const vector<pair<int, CubeMatrix>> offset_to_expected{
-        { 0, { 
-            { {0, 0, 1}, {1, 0, 1} },
-            { {0, 1, 1}, {1, 1} },
+    const vector<pair<BoardImpl, CubeMatrix>> board_to_expected{
+        { {{2}, 0}, { 
+            { {0, 0}, {1, 0} },
+            { {0, 1}, {1, 1} },
         }},
-        { 1, {
-            { {0, 1, 1}, {1, 1} },
-            { {0, 2}, {1, 2} },
+        { {{2}, 1}, {
+            { {0, 0}, {1, 0} },
+            { {0, 1}, {1, 1} },
         }},
-        { 2, {
-            { {0, 2}, {1, 2} },
-            { {0, 3}, {1, 3} },
+        { {{2}, 2}, {
+            { {0, 0}, {1, 0} },
+            { {0, 1}, {1, 1} },
         }},
     };
 
-    for (const auto& pair : offset_to_expected)
+    for (const auto& pair : board_to_expected)
     {
-        BoardImpl board{2, pair.first};
-        board.put_cubes(initial_cubes);
-
-        ASSERT_THAT(board.get_visible_cubes(), Eq(pair.second));
-    }    
+        const CubeMatrix& actual{pair.first.get_visible_cubes()};
+        
+        ASSERT_THAT(actual, Eq(pair.second));
+    }
 }
 
 TEST(BoardImpl, get_visible_brick_cubes)
 {
-    const vector<Cube> brick_cubes{ {0, 0, 1}, {1, 0, 1}, {1, 1, 1} };
-    const vector<pair<int, vector<Cube>>> offset_to_expected{
-        { 0, { {0, 0, 1}, {1, 0, 1}, {1, 1, 1} } },
-        { 1, { {1, 1, 1} } },
-        { 2, {} },
+    const BoardImpl initial_board{{2}, 2};
+    const vector<pair<vector<Cube>, vector<Cube>>> cubes_to_expected{
+        { {{0, 1}, {1, 1}, {1, -1}}, {{0, 1}, {1, 1}} },
+        { {{0, -2}, {1, 0}, {0, -2}}, {{1, 0}} },
+        { {{0, 1}, {1, 0}}, {{0, 1}, {1, 0}} },
     };
 
-    for (const auto& pair : offset_to_expected)
+    for (const auto& pair : cubes_to_expected)
     {
-        BoardImpl board{2, pair.first};
-        board.put_cubes(brick_cubes);
-        const vector<Cube>& actual{board.get_visible_brick_cubes(brick_cubes)};
+        const vector<Cube>& actual{
+            initial_board.get_visible_brick_cubes(pair.first)
+        };
 
         ASSERT_THAT(actual, Eq(pair.second));
     }    
