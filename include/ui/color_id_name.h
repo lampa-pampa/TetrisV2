e24db4b125ce85a21a86fcd007a31f1e1ed733fa
Lampa Pampa <marek.kandulski@proton.me>
2024-03-20 16:51:12 +0100

    removed enum to int map and replaced it with casting

--- include/ui/color_id_name.h
+++ include/ui/color_id_name.h
@@ -1,60 +1,34 @@
 #ifndef INCLUDE_UI_COLOR_ID_NAME_H
 #define INCLUDE_UI_COLOR_ID_NAME_H
 
-#include <cassert>
 #include <cstdint>
-#include <map>
 
 namespace Tetris::Ui
 {
 
 enum class ColorIdName: uint_fast8_t
 {
-    black,
-    dark_candy_apple_red,
-    islamic_green,
-    windsor_tan,
-    duke_blue,
-    heliotrope_magenta,
-    tiffany_blue,
-    dark_gray,
-    davy_s_grey,
-    sunset_orange,
-    screamin_green,
-    dodie_yellow,
-    very_light_blue,
-    shocking_pink,
-    electric_blue,
-    white,
+    black = 0,
+    dark_candy_apple_red = 1,
+    islamic_green = 2,
+    windsor_tan = 3,
+    duke_blue = 4,
+    heliotrope_magenta = 5,
+    tiffany_blue = 6,
+    dark_gray = 7,
+    davy_s_grey = 8,
+    sunset_orange = 9,
+    screamin_green = 10,
+    dodie_yellow = 11,
+    very_light_blue = 12,
+    shocking_pink = 13,
+    electric_blue = 14,
+    white = 15,
 };
 
-namespace
+constexpr uint_fast8_t get_color_id(ColorIdName name)
 {
-    const std::map<ColorIdName, uint_fast8_t> color_id_name_to_id
-    {
-        {ColorIdName::black, 0},
-        {ColorIdName::dark_candy_apple_red, 1},
-        {ColorIdName::islamic_green, 2},
-        {ColorIdName::windsor_tan, 3},
-        {ColorIdName::duke_blue, 4},
-        {ColorIdName::heliotrope_magenta, 5},
-        {ColorIdName::tiffany_blue, 6},
-        {ColorIdName::dark_gray, 7},
-        {ColorIdName::davy_s_grey, 8},
-        {ColorIdName::sunset_orange, 9},
-        {ColorIdName::screamin_green, 10},
-        {ColorIdName::dodie_yellow, 11},
-        {ColorIdName::very_light_blue, 12},
-        {ColorIdName::shocking_pink, 13},
-        {ColorIdName::electric_blue, 14},
-        {ColorIdName::white, 15},
-    };
-}
-
-inline uint_fast8_t get_color_id(ColorIdName name)
-{
-    assert(color_id_name_to_id.find(name) != color_id_name_to_id.end());
-    return color_id_name_to_id.at(name);
+    return static_cast<uint_fast8_t>(name);
 }
 
 }
