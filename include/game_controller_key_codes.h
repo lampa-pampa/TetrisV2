ada703b13af5037aa0a0aec86fde0b843680f346
Lampa Pampa <marek.kandulski@proton.me>
2024-04-06 23:42:48 +0200

    put game_controller_key_codes to Tetris namespace

--- include/game_controller_key_codes.h
+++ include/game_controller_key_codes.h
@@ -1,11 +1,16 @@
 #ifndef INCLUDE_GAME_CONTROLLER_KEY_CODES
 #define INCLUDE_GAME_CONTROLLER_KEY_CODES
 
+namespace Tetris
+{
+
 struct GameControllerKeyCodes final
 {
     int pause;
     int quit;
     int no_key;
 };
+
+}
 
 #endif
