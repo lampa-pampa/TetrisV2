9a88a1e163bb19dee9c7fc3a6d53f818c22ca4f0
Lampa Pampa <marek.kandulski@proton.me>
2024-05-04 23:06:16 +0200

    renamed color maps

--- include/ui/matrix_display/console/ncurses_colors.h
+++ include/ui/matrix_display/console/ncurses_colors.h
@@ -11,9 +11,9 @@ class NCursesColors final
 {
 public:
     NCursesColors(
-        const std::map<uint_fast8_t, uint_fast8_t>& color_id_to_ncurses_color)
+        const std::map<uint_fast8_t, uint_fast8_t>& color_id_to_ncurses_id)
     :
-        color_id_to_ncurses_color_{color_id_to_ncurses_color}
+        color_id_to_ncurses_id_{color_id_to_ncurses_id}
     {}
 
     int get_ncurses_color(uint_fast8_t color_id);
@@ -22,9 +22,9 @@ private:
     using ColorPair = std::_Rb_tree_iterator<
         std::pair<const uint_fast8_t, uint_fast8_t>>;
     
-    const std::map<uint_fast8_t, uint_fast8_t> color_id_to_ncurses_color_;
+    const std::map<uint_fast8_t, uint_fast8_t> color_id_to_ncurses_id_;
     
-    std::map<uint_fast8_t, uint_fast8_t> color_to_pair_;
+    std::map<uint_fast8_t, uint_fast8_t> color_id_to_ncurses_color_;
 
     ColorPair create_color_pair(uint_fast8_t color_id);
 };
--- src/ui/matrix_display/console/ncurses_colors.cpp
+++ src/ui/matrix_display/console/ncurses_colors.cpp
@@ -10,8 +10,8 @@ namespace Tetris::Ui
 
 int NCursesColors::get_ncurses_color(uint_fast8_t color_id)
 {
-    auto it{color_to_pair_.find(color_id)};
-    if (it == color_to_pair_.end())
+    auto it{color_id_to_ncurses_color_.find(color_id)};
+    if (it == color_id_to_ncurses_color_.end())
         it = create_color_pair(color_id);
     return it->second;
 }
@@ -21,10 +21,10 @@ int NCursesColors::get_ncurses_color(uint_fast8_t color_id)
 NCursesColors::ColorPair NCursesColors::create_color_pair(
     uint_fast8_t color_id)
 {
-    auto it{color_id_to_ncurses_color_.find(color_id)};
-    assert(it != color_id_to_ncurses_color_.end());
+    auto it{color_id_to_ncurses_id_.find(color_id)};
+    assert(it != color_id_to_ncurses_id_.end());
     ::init_pair(it->second, it->second, COLOR_BLACK);
-    return color_to_pair_.insert({color_id, it->second}).first;
+    return color_id_to_ncurses_color_.insert({color_id, it->second}).first;
 }
 
 }
