a553f883fa03229d9f2134aa495c2c39cee6736d
Lampa Pampa <marek.kandulski@proton.me>
2024-05-05 14:00:20 +0200

    added led_matrix_display_impl and it's creator function

---
+++ include/ui/matrix_display/led/led_matrix_display_impl.h
@@ -0,0 +1,48 @@
+#ifndef INCLUDE_UI_LED_MATRIX_DISPLAY_IMPL_H
+#define INCLUDE_UI_LED_MATRIX_DISPLAY_IMPL_H
+
+#include "ui/matrix_display/matrix_display.h"
+
+#include <map>
+
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+
+#include "ui/color/iv_color.h"
+#include "vector_2/vector_2.h"
+
+namespace Tetris::Ui
+{
+
+class LedMatrixDisplayImpl final: public MatrixDisplay
+{  
+public:
+    LedMatrixDisplayImpl(
+        Vector2 size,
+        int chain_length,
+        HUB75_I2S_CFG::i2s_pins pins,
+        const std::map<uint_fast8_t, uint_fast8_t>& color_id_to_hs_color
+    );
+
+    void refresh(const IvColorMatrix& colors) override;
+
+    int get_pressed_key_code() override
+    {
+        return -1;
+    }
+
+    Vector2 get_size() const override
+    {
+        return size_;
+    }
+
+private:
+    const Vector2 size_;
+    const std::map<uint_fast8_t, uint_fast8_t> color_id_to_hs_color_;
+    MatrixPanel_I2S_DMA matrix_;
+
+    void refresh_pixel(Vector2 position, IvColor color);
+};
+
+}
+
+#endif
---
+++ src/ui/matrix_display/led/led_matrix_display_impl.cpp
@@ -0,0 +1,54 @@
+#include "ui/matrix_display/led/led_matrix_display_impl.h"
+
+#include <cstdint>
+
+#include <boost/range/irange.hpp>
+
+#include "vector_2/vector_2.h"
+#include "ui/color/iv_color.h"
+
+using boost::irange;
+
+namespace Tetris::Ui
+{
+
+LedMatrixDisplayImpl::LedMatrixDisplayImpl(
+    Vector2 size,
+    int chain_length,
+    HUB75_I2S_CFG::i2s_pins pins,
+    const std::map<uint_fast8_t, uint_fast8_t>& color_id_to_hs_color)
+:
+    size_{size},
+    color_id_to_hs_color_{color_id_to_hs_color},
+    matrix_{{
+        static_cast<uint16_t>(size.x),
+        static_cast<uint16_t>(size.y),
+        static_cast<uint16_t>(chain_length),
+        pins,
+    }}
+{
+    matrix_.begin();
+}
+
+void LedMatrixDisplayImpl::refresh(const IvColorMatrix& colors)
+{
+    for (const auto& y : irange(size_.y))
+    {
+        for (const auto& x : irange(size_.x))
+            refresh_pixel({x, y}, colors[y][x]);
+    }
+}
+
+//-----------------------------------------------------------------------
+
+void LedMatrixDisplayImpl::refresh_pixel(Vector2 position, IvColor color)
+{
+    uint16_t rgb_color;
+    if(color.id == 0)
+        rgb_color = matrix_.color565(0, 0, 0);
+    else
+        rgb_color = matrix_.color565(0, 0, 255);
+    matrix_.drawPixel(position.x, position.y, rgb_color);
+}
+
+}
