657f0669909aa3cd338aec31ba9121ca3b216a73
Lampa Pampa <marek.kandulski@proton.me>
2024-05-13 13:21:49 +0200

    moved from_hsv function from cpp file to header because it's constexpr

--- include/ui/color/led/rgb_color.h
+++ include/ui/color/led/rgb_color.h
@@ -14,8 +14,32 @@ struct RgbColor final
     uint_fast8_t green;
     uint_fast8_t blue;
 
-    constexpr static RgbColor from_hsv(HsColor hs_color, uint_fast8_t value);
+    constexpr static RgbColor from_hsv(HsColor hs_color, uint_fast8_t value)
+    {
+        if (hs_color.saturation == 0)
+            return {value, value, value};
 
+        const uint_fast8_t region(hs_color.hue / 43);
+        const uint_fast8_t remainder((hs_color.hue - region * 43) * 6);
+        
+        const uint_fast8_t p = value
+            * (0xff - hs_color.saturation) >> 8;
+        const uint_fast8_t q = value
+            * (0xff - (hs_color.saturation * remainder >> 8)) >> 8;
+        const uint_fast8_t t = value
+            * (0xff - (hs_color.saturation * (0xff - remainder) >> 8)) >> 8;
+        
+        switch (region)
+        {
+            case 0: return {value, t, p};
+            case 1: return {q, value, p};
+            case 2: return {p, value, t};
+            case 3: return {p, q, value};
+            case 4: return {t, p, value};
+            default: return {value, p, q};
+        }
+    }
+    
     constexpr RgbColor(uint_fast8_t red, uint_fast8_t green, uint_fast8_t blue)
     :
         red{red},
