7127bfa3f69b46c10e077f84ee9414f357f9d24e
Lampa Pampa <123573246+lampa-pampa@users.noreply.github.com>
2024-05-19 22:06:09 +0200

    updated led main - added input_config

--- src/main/led/main.cpp
+++ src/main/led/main.cpp
@@ -1,11 +1,13 @@
 #include "config/filled_config.h"
 #include "config/matrix_display/led/filled_matrix_config.h"
+#include "config/input_receiver/led/filled_input_config.h"
 #include "run_tetris/run_tetris.h"
 #include "ui/input_receiver/led/led_input_receiver_impl.h"
 #include "ui/matrix_display/led/led_matrix_display_impl.h"
 
 using Tetris::config;
 using Tetris::matrix_config;
+using Tetris::input_config;
 using Tetris::run_tetris;
 using Tetris::Ui::LedInputReceiverImpl;
 using Tetris::Ui::LedMatrixDisplayImpl;
@@ -13,13 +15,9 @@ using Tetris::Ui::LedMatrixDisplayImpl;
 void setup()
 {
     LedMatrixDisplayImpl matrix{matrix_config};
-    LedInputReceiverImpl input_receiver{};
+    LedInputReceiverImpl input_receiver{input_config};
 
-    run_tetris(
-        config,
-        input_receiver,
-        matrix
-    );
+    run_tetris(config, input_receiver, matrix);
 }
 
 void loop() {}
