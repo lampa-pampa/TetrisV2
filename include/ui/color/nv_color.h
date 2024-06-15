a6f87de95db28153be320bedbdedb3cd570b444f
Lampa Pampa <123573246+lampa-pampa@users.noreply.github.com>
2024-06-15 19:35:44 +0200

    added ostream operator to nv_color

--- include/ui/color/nv_color.h
+++ include/ui/color/nv_color.h
@@ -2,8 +2,11 @@
 #define INCLUDE_UI_NV_COLOR_H
 
 #include <cstdint>
+#include <iomanip>
+#include <ostream>
 
 #include "ui/color/color_name.h"
+#include "utils/uint_fast8_t_ostream.h"
 
 namespace Tetris::Ui
 {
@@ -22,6 +25,13 @@ struct NvColor final
     bool operator==(const NvColor& other) const
     {
         return name == other.name and value == other.value;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const NvColor& color)
+    {
+        return os << "{" << color.name << ", 0x" << std::hex << std::setw(2)
+                  << std::setfill('0') << color.value << std::dec
+                  << std::setfill(' ') << std::setw(0) << "}";
     }
 };
 
